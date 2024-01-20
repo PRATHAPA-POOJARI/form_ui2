1165ed0b0362dec6229072f23aaaf56e0023339c
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2024-01-20 18:10:43 +0530

    Update VendorList.js

--- form_ui2/src/components/VendorList.js
+++ form_ui2/src/components/VendorList.js
@@ -1,7 +1,8 @@
-// VendorList.js
+
 import React from 'react';
 import { Paper, Typography, Button } from '@mui/material';
-import VendorViewForm from './VendorViewForm'; // Import the VendorViewForm component
+import VendorViewForm from './VendorViewForm'; 
+import VendorEditForm from './VendorEditForm'; 
 
 const VendorListItem = ({ vendor, onDelete, onView, onEdit }) => (
   <Paper key={vendor._id} style={{ padding: 10, maxWidth: 400, margin: 'auto', marginTop: 10, border: '1px solid #ccc', borderRadius: 8 }}>
@@ -9,21 +10,21 @@ const VendorListItem = ({ vendor, onDelete, onView, onEdit }) => (
     <Typography variant="body1">{`Bank Account No: ${vendor.bankAccountNo}`}</Typography>
     <Typography variant="body1">{`Bank Name: ${vendor.bankName}`}</Typography>
 
-    <Button onClick={() => onDelete(vendor._id)} variant="outlined" color="error" sx={{marginLeft:2}}>
+    <Button onClick={() => onDelete(vendor._id)} variant="outlined" color="error" sx={{ marginLeft: 2 }}>
       Delete
     </Button>
 
-    {/* Use VendorViewForm when "View Vendor" is clicked */}
-    <Button onClick={() => onView(vendor)} variant="outlined" color="primary" sx={{marginLeft:2}}>
+    <Button onClick={() => onView(vendor)} variant="outlined" color="primary" sx={{ marginLeft: 2 }}>
       View Vendor
     </Button>
 
-    <Button onClick={() => onEdit(vendor)} variant="outlined" color="secondary" sx={{marginLeft:2}}>
+    <Button onClick={() => onEdit(vendor)} variant="outlined" color="secondary" sx={{ marginLeft: 2 }}>
       Edit Vendor
     </Button>
 
-    {/* Render the VendorViewForm for the selected vendor */}
-    {vendor.isViewing && <VendorViewForm vendor={vendor} />}
+  
+    {vendor.isEditing && <VendorEditForm vendor={vendor} onEdit={onEdit} />}
+    {vendor.isViewing && <VendorViewForm vendor={vendor} />} 
   </Paper>
 );
 
